11199ec1e7b270582e07505b0ca5c47acb0b2ebf
unknown <shahinhasanov_@hotmail.comgit config --global user.name>
2024-01-25 20:16:37 +0400

    created feedmodel,migrations, admin.py

--- newsfeed/feeds/models.py
+++ newsfeed/feeds/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Feed(models.Model):
+    title = models.CharField(max_length=256)
+    body = models.TextField(max_length=3000)
+    published_at = models.DateTimeField(auto_now_add=True)
+    views_count =models.PositiveIntegerField(default=0, editable=False)
+
+    def __str__(self) -> str:
+        return f"{self.title[:10]}..."
